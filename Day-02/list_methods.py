7680cf666a228ed94172434e7fb5d31608cb4b82
Vedant chhatrola <110011435+Vedantpatel2006@users.noreply.github.com>
2026-07-07 18:55:36 +0530

    Implement various list methods in list_methods.py

---
+++ Day-02/list_methods.py
@@ -0,0 +1,28 @@
+list1 = [2,3,4,5,6,66,77,88,99]
+
+print(list1)
+
+list1.append(111)
+print("append:", list1)
+
+list1.extend(["ch","tn"])
+print("extend:", list1)
+
+list1.insert(2,"kaiser")
+print("insert:", list1)
+
+list1.remove(66)
+print("remove:", list1)
+
+list1.pop()
+print("pop:", list1)
+
+print("index:", list1.index(77))
+
+print("count:", list1.count(88))
+
+list1.sort()
+print("sort:", list1)
+
+list1.reverse()
+print("reverse:", list1)
